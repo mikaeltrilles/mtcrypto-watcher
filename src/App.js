437b94a8a2892570fea4af619609deb0395c9c00
Mikael Trilles <11914687-mkaeltrilles@users.noreply.gitlab.com>
2022-07-11 18:04:33 +0200

    👌 IMPROVE: Table-header is OK

--- src/App.js
+++ src/App.js
@@ -19,14 +19,30 @@ const App = () => {
   useEffect(() => {
     axios
       .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y')
-      .then((res) => setCoinsData(res.data))
+      .then((res) => setCoinsData(res.data));
+
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 145) {
+
+        //* 🇫🇷 Si le scroll est supérieur à 145px, le 'table-header' est placé en haut de l'écran - 🇺🇸 If the scroll is greater than 145px, the 'table-header' is placed at the top of the screen
+
+        document.querySelector(".table-header").classList.add("active");
+
+      } else {
+
+        //* 🇫🇷 Si le scroll est inférieur à 145px, le 'table-header' est retiré du haut de l'écran - 🇺🇸 If the scroll is less than 145px, the 'table-header' is removed from the top of the screen
+
+        document.querySelector(".table-header").classList.remove("active");
+      }
+    });
   }, []);
 
   return (
     <div className="app-container">
       <header>
         <HeaderInfos />
-        {/* * Je passe les données (data) des monnaies (coins) en props */}
+        {/* 🇫🇷 Les données (data) des monnaies (coins) sont passées en props - 🇺🇸 The data (coins) of the coins are passed as props */}
         <GlobalChart coinsData={coinsData} />
         <p>{Date()}</p>
       </header>
