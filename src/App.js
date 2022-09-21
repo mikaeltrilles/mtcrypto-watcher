75732b3cad9e733f731a0ee2f5feaa002af12cd0
Mikael Trilles <mikaeltrillespro@outlook.com>
2022-09-21 22:27:02 +0200

    📖 DOC:

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
 
 
-  //* Récupération des données de l'API avec axios
+  //* Récupération des données de l'API avec axios, on récupère l'intégralité des données en un seul call
   useEffect(() => {
     axios
       .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y')
